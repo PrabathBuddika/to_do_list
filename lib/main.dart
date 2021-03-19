8a7bf380361b749bc19fd2042b0db721aae63d89
waimaa <prabth0755@gmail.com>
2021-03-19 18:23:42 +0530

    init

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          textTheme: GoogleFonts.nunitoSansTextTheme(
+        Theme.of(context).textTheme,
+      )),
+      home: Scaffold(
+        body: Container(
+          child: Center(
+              child: Image(
+            image: AssetImage('assets/images/logo.png'),
+          )),
+        ),
+      ),
+    );
+  }
+}
